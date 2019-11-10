d1ec7bc80fa344819a182e8f3f9576c386238ead
Paco <fjfdepedro@gmail.com>
2019-11-10 19:50:07 +0100

    add tests

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
@@ -1,6 +1,5 @@
 package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 
-import es.urjccode.mastercloudapps.adcs.draughts.models.Error;
 import es.urjccode.mastercloudapps.adcs.draughts.models.*;
 import org.junit.Test;
 
@@ -19,9 +18,9 @@ public class PlayControllerTest {
     }
 
     @Test
-    public void givenPlayControllerWhenMovementRequiereCorrectThenNotError() {
-        Coordinate origin = new Coordinate(5, 0);
-        Coordinate target = new Coordinate(4, 1);
+    public void givenPlayControllerGoodMovemment() {
+        Coordinate origin = new Coordinate(6, 1);
+        Coordinate target = new Coordinate(5, 2);
         assertNull(playController.move(origin, target));
         assertNull(playController.getPiece(origin));
         Piece pieceTarget = playController.getPiece(target);
@@ -29,34 +28,4 @@ public class PlayControllerTest {
         assertEquals(pieceTarget.getColor(), Color.WHITE);
     }
 
-    private Error advance(Coordinate[][] coordinates){
-        Error error = null;
-        for (int i = 0; i < coordinates.length; i++) {
-            assertNull(error);
-            error = playController.move(coordinates[i][0], coordinates[i][1]);
-        }
-        return error;
-    }
-
-    private void boardWithOneBlackPiece(){
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < Board.DIMENSION; j++) {
-                Coordinate coordinate = new Coordinate(i,j);
-                Piece piece= board.getPiece(coordinate);
-                if (!coordinate.equals(new Coordinate(2,3)) && piece != null){
-                    board.remove(coordinate);
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testGivenGameWhenMovementEatLastPieceThenOk() {
-        boardWithOneBlackPiece();
-        assertNull(this.advance(new Coordinate[][] {
-            { new Coordinate(5, 0), new Coordinate(4, 1) },
-            { new Coordinate(2, 3), new Coordinate(3, 2) },
-            { new Coordinate(4, 1), new Coordinate(2, 3) },
-        }));
-    }
 }
--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/views/CommandViewTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/views/CommandViewTest.java
@@ -4,16 +4,18 @@ import es.urjccode.mastercloudapps.adcs.draughts.controllers.PlayController;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Color;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
 import es.urjccode.mastercloudapps.adcs.draughts.utils.Console;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class CommandViewTest {
@@ -27,17 +29,27 @@ public class CommandViewTest {
     @InjectMocks
     CommandView commandView;
 
-    @Before
-    public void initMocks() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
-    public void testInteract() {
+    public void testCommandViewGoodMovementBlack(){
         when(playController.getColor()).thenReturn(Color.BLACK);
         when(console.readString("Mueven las negras: ")).thenReturn("21.30\n");
         commandView.interact();
-        verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
+        verify(playController).move(new Coordinate(2,1), new Coordinate(3, 0));
+    }
+    @Test
+    public void testCommandViewGoodMovementWhite(){
+        when(playController.getColor()).thenReturn(Color.WHITE);
+        when(console.readString("Mueven las blancas: ")).thenReturn("61.52\n");
+        commandView.interact();
+        verify(playController).move(new Coordinate(6, 1), new Coordinate(5, 2));
+    }
+
+    @Test
+    public void testCommandViewBadMovement(){
+        when(playController.getColor()).thenReturn(Color.BLACK);
+        when(console.readString(anyString())).thenReturn("21.RR.90");
+        commandView.interact();
+        verify(console).writeln(Mockito.matches("(.*error)"));
     }
 
 }
